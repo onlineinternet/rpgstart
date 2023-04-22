1fabd5f6b374e39b8aa9140f23590db31d11f269
onlineinternet <ochobobo@gmail.com>
2023-04-22 18:29:09 -0400

    fixing background image path

--- scripts/startpage.js
+++ scripts/startpage.js
@@ -31,7 +31,7 @@ function getRandomMessage() {
   return messages[randomIndex];
 }
 
-var repoName = 'https://onlineinternet.github.io/rpgstart';
+var repoName = '/rpgstart';
 var imageURLs = [];
 
 for (var i = 1; i <= 20; i++) {
